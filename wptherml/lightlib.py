16fac304ff62ec9867255f8503be7d6de0aadda6
FoleyLab <jayfoleyiv@gmail.com>
2019-02-19 12:05:00 -0500

    added a test script

--- wptherml/lightlib.py
+++ wptherml/lightlib.py
@@ -17,8 +17,45 @@ This function passed the following tests:
 from wptherml.numlib import numlib
 from wptherml.datalib import datalib
 import numpy as np
-from matplotlib import pyplot as plt
 
+def Lum_efficiency(lam, TE):
+    upper = np.amax(lam)
+    ph = datalib.PhLum(lam)
+    num = ph*TE
+    numerator = numlib.Integrate(num, lam, 0, upper )
+    den = TE
+    denominator = numlib.Integrate(den, lam, 0, upper)
+    return (numerator/denominator)
+
+
+def Lum_efficiency_prime(lam, TE, TE_prime):
+    upper = np.amax(lam)
+    ph = datalib.PhLum(lam)
+    num = ph*TE
+    num_prime = ph*TE_prime
+    numerator = numlib.Integrate(num, lam, 0, upper )
+    numerator_prime = numlib.Integrate(num_prime, lam, 0, upper)
+    
+    denominator = numlib.Integrate(TE, lam, 0, upper)
+    denominator_prime = numlib.Integrate(TE_prime, lam, 0, upper)
+    
+    return (denominator*numerator_prime - numerator*denominator_prime)/(denominator**2)
+
+def Lum_efficacy(lam, TE):
+    le = Lum_efficiency(lam, TE)
+    efficacy = 683*le
+    return efficacy
+
+def IdealSource(lam, T):
+    ### compute blackbody spectrum at current T
+    rho = datalib.BB(lam, T)
+    ### get photopic luminosity function
+    ph  = datalib.PhLum(lam)
+    ### ideal thermal emission is product of the two
+    TE_ideal = ph * rho
+    return TE_ideal
+
+'''
     
 def luminous_efficiency(lam, thermal_emission_array):
     upper = np.amax(lam)
@@ -43,3 +80,4 @@ def thermal_emission_ideal_source(lam, T):
     ### ideal thermal emission is product of the two
     thermal_emission_ideal_source = ph * rho
     return thermal_emission_ideal_source_array
+'''
